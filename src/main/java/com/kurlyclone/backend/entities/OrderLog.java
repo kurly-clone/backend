5c75b9e0c36217eb23bcb69d51d4502d953adb52
Jae Min Kim <jmk1806@korea.ac.kr>
2023-08-17 02:22:47 +0900

    FEAT : order_log 테이블 생성

---
+++ src/main/java/com/kurlyclone/backend/entities/OrderLog.java
@@ -0,0 +1,57 @@
+package com.kurlyclone.backend.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderLog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Enumerated(EnumType.ORDINAL)
+	@Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+	private OrderStatus status;
+
+	public enum OrderStatus {
+		READY, COMPLETED, IN_TRANSIT, DELIVERED
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product productId;
+
+	@Column(nullable = false)
+	private int amount;
+
+	@Column(nullable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
