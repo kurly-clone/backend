88c581a6ba136123f6d3dc9f25383cca3faab9e3
Jae Min Kim <jmk1806@korea.ac.kr>
2023-08-16 19:27:44 +0900

    FEAT : favorite 테이블 생성

---
+++ src/main/java/com/kurlyclone/backend/entities/Favorite.java
@@ -0,0 +1,27 @@
+package com.kurlyclone.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Favorite {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product productId;
+}
