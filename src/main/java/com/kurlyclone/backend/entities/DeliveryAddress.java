b1e3f5686680c127a1a1636bdb5431336b4fc2c1
Jae Min Kim <jmk1806@korea.ac.kr>
2023-08-16 19:49:32 +0900

    FEAT : delivery_adress 테이블 생성

---
+++ src/main/java/com/kurlyclone/backend/entities/DeliveryAddress.java
@@ -0,0 +1,31 @@
+package com.kurlyclone.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class DeliveryAddress {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+
+	@Column(nullable = false)
+	private String address;
+	@Column(nullable = false)
+	@Builder.Default
+	private Boolean isBasic = false;
+}
