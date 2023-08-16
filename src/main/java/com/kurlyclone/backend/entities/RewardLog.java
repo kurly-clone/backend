5a245eb81e3b72e409c1e02915f80afe20dc49c9
Jae Min Kim <jmk1806@korea.ac.kr>
2023-08-17 02:34:08 +0900

    FEAT : reward_log 테이블 생성

---
+++ src/main/java/com/kurlyclone/backend/entities/RewardLog.java
@@ -0,0 +1,38 @@
+package com.kurlyclone.backend.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class RewardLog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int amount;
+
+	@Column(nullable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+}
